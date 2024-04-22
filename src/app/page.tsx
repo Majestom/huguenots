4f246f33c399e7f609e526d103eae1e848426139
Tom <toalcoja@gmail.com>
2024-04-22 21:44:18 +0100

    Remove redundant code from group structures.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,6 @@ const marketAndRegionGroup = [
       { label: "Asia Pacific" },
       {
         label: "Europe",
-        selected: true,
         subOptions: [
           { label: "Eurozone" },
           { label: "Germany" },
@@ -35,7 +34,6 @@ const marketAndRegionGroup = [
       { label: "Global" },
       {
         label: "North America",
-        selected: true,
         subOptions: [{ label: "United States" }],
       },
     ],
